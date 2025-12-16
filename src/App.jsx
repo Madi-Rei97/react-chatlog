fa099915419bd61c8a87d53fb93af157603f09ef
Madi Rei <madison.nicole.rei@gmail.com>
2025-12-15 17:14:16 -0800

    adds single chat message bubble (wave 1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry.jsx';
+import messages from './data/messages.json';
 
 const App = () => {
+  const firstMessage = messages[0];
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat Between Vladimir and Estragon</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={firstMessage.sender}
+          body={firstMessage.body}
+          timeStamp={firstMessage.timeStamp}
+        />
+        {/* Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
